f85290111e4c3b9b4c7ca65b20e910e22f1b95e0
Logan McCalley <mccalleylogan@gmail.com>
2022-12-07 16:24:22 -0500

    working and connected to front end

---
+++ functions/src/routes/audio.js
@@ -0,0 +1,11 @@
+import dbConnect from "../dbConnect.js";
+
+export async function getAllAudio(req, res) {
+    const db = dbConnect();
+    const collection = await db.collection("jlSpeakers").find().toArray()
+      .catch(err => {
+        res.status(500).send(err);
+         return;
+      });
+    res.send(collection);
+  }
